6325cad7dbaf15f7a187a3a19c690274db982d61
Tester <test7@user.com>
2021-11-16 11:41:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            double A = 10, B = 20, k, i;
+            for (i = A; i <= B; i++)
+            {
+
+                k = Math.Pow(i, 2);
+                Console.WriteLine($"{i} в квадрате ={k}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
